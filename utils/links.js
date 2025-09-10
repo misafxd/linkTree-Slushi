17ba80e962ee0a027fa09b038c25e5658393cb5e
misafxd <pisichu.mafs@gmail.com>
2025-09-10 12:44:09 -0600

    Add correct links to utils/links

--- utils/links.js
+++ utils/links.js
@@ -1,37 +1,37 @@
 export const links = [
   {
     titulo: "Twitch",
-    link: "#",
+    link: "https://www.twitch.tv/slushichan",
     src: "./assets/icons/twitch.svg",
   },
   {
     titulo: "Instagram",
-    link: "#",
+    link: "https://www.instagram.com/slushi_chan/",
     src: "./assets/icons/instagram.svg",
   },
   {
     titulo: "Tiktok",
-    link: "#",
+    link: "https://www.tiktok.com/@slushichan",
     src: "./assets/icons/tiktok.svg",
   },
   {
     titulo: "Discord",
-    link: "#",
+    link: "https://discord.gg/3x359mH8aM",
     src: "./assets/icons/discord.svg",
   },
   {
     titulo: "Kofi",
-    link: "#",
+    link: "https://ko-fi.com/slushichan",
     src: "./assets/icons/kofi.svg",
   },
   {
     titulo: "Youtube",
-    link: "#",
+    link: "https://www.youtube.com/@Slushi-chan",
     src: "./assets/icons/youtube.svg",
   },
   {
     titulo: "Wishlist",
-    link: "#",
+    link: "https://www.amazon.com.mx/hz/wishlist/ls/D7DLQIXJR8JY?ref_=wl_share",
     src: "./assets/icons/amazon.svg",
   },
 ];
